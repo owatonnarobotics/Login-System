06ecc5dc57d0fbcc26d36432ea1734d4c6c0ceee
unknown <ethan@eschulz.com>
2014-07-04 01:18:56 -0500

    Added basic sign in page.

--- src/com/owatonnarobotics/User.java
+++ src/com/owatonnarobotics/User.java
@@ -11,5 +11,37 @@ package com.owatonnarobotics;
  * @author Eson
  */
 public class User {
+    private String firstName;
+    private String secondName;
+    private String id;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
     
+    public User(String fName, String lName, String num){
+        firstName = fName;
+        secondName = lName;
+        id = num;
+    }
 }
