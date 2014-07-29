95f4d82598f8aa966b4ea2f6b53a1caf6e7f63ee
unknown <ethan@eschulz.com>
2014-07-29 01:06:29 -0500

    Added ability to get the user from the Excel file

--- src/com/owatonnarobotics/ExcelManager.java
+++ src/com/owatonnarobotics/ExcelManager.java
@@ -1,15 +1,48 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.owatonnarobotics;
+
+import java.io.File;
+import java.io.IOException;
+import jxl.Cell;
+import jxl.Sheet;
+import jxl.Workbook;
+import jxl.read.biff.BiffException;
+import jxl.write.WritableWorkbook;
 
 /**
  *
  * @author Eson
  */
 public class ExcelManager {
+    private static final String excelLocation = "time_log.xls";
     
+    //Lists the different column values
+    private static final int idColumn = 0;
+    private static final int fNameColumn = 1;
+    private static final int lNameColumn = 2;
+    private static final int teamColumn = 3;
+    private static final int totalTimeColumn = 5;
+    private static final int datesStartColumn = 8;
+    
+    private static final int rowStart = 2;
+    
+    public static User getUser(String id) throws IOException, BiffException{
+        Workbook workbook = Workbook.getWorkbook(new File(excelLocation));
+        
+        Sheet sheet = workbook.getSheet(0);
+        
+        for(int currentRow = rowStart; currentRow < sheet.getRows(); currentRow ++){
+            Cell cell = sheet.getCell(0, currentRow);
+            if(cell.getContents().equals(id)){
+                String fName = sheet.getCell(fNameColumn, currentRow).getContents();
+                String lName = sheet.getCell(lNameColumn, currentRow).getContents();
+                
+                workbook.close();
+                return new User(fName, lName, id);
+            }
+            currentRow += 1;
+        }
+        
+        workbook.close();
+        return null;
+    }
 }
