19e2de14b74880d6411269d9117901dd2658a557
unknown <ethan@eschulz.com>
2014-08-02 18:54:58 -0500

    Added createUser which adds if the user is signed in/out to a prop file

--- src/com/owatonnarobotics/LoginManager.java
+++ src/com/owatonnarobotics/LoginManager.java
@@ -1,9 +1,30 @@
 package com.owatonnarobotics;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Properties;
+
 /**
  * Edits the properties file that will contain 
  * @author Eson
  */
 public class LoginManager {
     
+    private static String propLocation = "login.properties";
+    
+    public static void createUser(String id) throws FileNotFoundException, IOException{
+        
+        Properties prop = new Properties();
+        OutputStream output = null;
+        
+        output = new FileOutputStream(propLocation);
+        
+        prop.setProperty(id, "out");
+        
+        prop.store(output, null);
+        
+        output.close();
+    }
 }
