a42d854d5019164e1f709690f71162795e1521b6
unknown <ethan@eschulz.com>
2014-08-02 19:16:17 -0500

    Added userSignedIn method, tells if the user is signed in

--- src/com/owatonnarobotics/LoginManager.java
+++ src/com/owatonnarobotics/LoginManager.java
@@ -1,8 +1,10 @@
 package com.owatonnarobotics;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Properties;
 
@@ -16,15 +18,23 @@ public class LoginManager {
     
     public static void createUser(String id) throws FileNotFoundException, IOException{
         
-        Properties prop = new Properties();
-        OutputStream output = null;
+        Properties property = new Properties();
+        OutputStream output = new FileOutputStream(PROP_LOCATION);
         
-        output = new FileOutputStream(PROP_LOCATION);
+        property.setProperty(id, "out");
         
-        prop.setProperty(id, "out");
-        
-        prop.store(output, null);
+        property.store(output, null);
         
         output.close();
+    }
+    
+    public static boolean userSignedIn(String id) throws FileNotFoundException, IOException{
+        
+        Properties property = new Properties();
+        InputStream input = new FileInputStream(PROP_LOCATION);
+        
+        property.load(input);
+        
+        return ! property.getProperty(id).equals("out");
     }
 }
